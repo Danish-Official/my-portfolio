d01b0f01c6408f93faa9dee90e0358f38cbcd669
Danish_Official <danishsidd011@gmail.com>
2024-10-06 13:29:12 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: '0.0.0.0',             // Bind to 0.0.0.0 for external access
+    port: process.env.PORT || 5173,  // Use the port provided by Render or fallback to 5173
+  },
+});
