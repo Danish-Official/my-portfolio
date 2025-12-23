ab829a1dc70956771ddcc27e495b53a74a9b6519
Danish-Official <96756569+Danish-Official@users.noreply.github.com>
2025-12-23 09:57:54 +0530

    Update copyright year in Footer component

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
         </a>
       </div>
 
-      <p className="text-white-500">© 2024 Danish Siddiqui. All rights reserved.</p>
+      <p className="text-white-500">© 2025 Danish Siddiqui. All rights reserved.</p>
     </footer>
   );
 };
